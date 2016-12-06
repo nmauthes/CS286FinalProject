308b40329786dd8210a1565f75803d0dd9ecf686
jchee <jasonchee@live.com>
2016-12-05 20:52:14 -0800

    fixed emission matrix

--- CS286 Final Project/src/generate_chords.py
+++ CS286 Final Project/src/generate_chords.py
@@ -3,10 +3,13 @@ import yahmm
 # import model
 file = open('model.txt', 'r')
 model = yahmm.Model.read(file)
+file.close()
 
 # use imported model to generate chords based on roboccini output
-sequence = ['C', 'G']
+sequence = ['C', 'A', 'F', 'D' ]
 logp, path = model.viterbi( sequence )
-print(path)
-for state in path:
-    print(state[1].name)
+if path is not None:
+    for state in path:
+        print(state[1].name)
+else:
+    print('No path found')
